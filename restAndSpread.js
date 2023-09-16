bc5db3b2747ed14cf4dbec08850922eaa96aed49
Noah Mekonnen <noahmekonnen0@gmail.com>
2023-09-16 12:19:39 -0400

    true finishing touches

--- restAndSpread.js
+++ restAndSpread.js
@@ -31,39 +31,47 @@ function doubleAndReturnArgs(arr,...nums){
 /** remove a random element in the items array
 and return a new array without that item. */
 
-// function removeRandom(...items) {
-//     let randomIndex = Math.floor(Math.random()*items.length)
-//     items.splice(randomIndex,1)
-//     const finalItems = items
-//     return finalItems
-// }
 
-function removeRandom(...items){
-    
+const removeRandom = items => {
+    items.splice(Math.floor(Math.random()*items.length),1)
+    return items
 }
 
 /** Return a new array with every item in array1 and array2. */
 
-function extend(array1, array2) {
-    return [...array1,...array2]
-}
+const extend = (array1, array2) => [...array1,...array2]
+
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
-function addKeyVal(obj, key, val) {
+// function addKeyVal(obj, key, val) {
+//     if (!obj[key])
+//     {
+//         obj[key] = val
+//         return obj
+//     }
+//     return 'it be there already'
+// }
+
+const addKeyVal = (obj, key, val) => {
     if (!obj[key])
-    {
+        {
         obj[key] = val
         return obj
-    }
+        }
     return 'it be there already'
 }
 
 
 /** Return a new object with a key removed. */
 
-function removeKey(obj, key) {
+// function removeKey(obj, key) {
+//     delete obj[key]
+//     return obj
+// }
+
+const removeKey = (obj, key) => {
     delete obj[key]
     return obj
 }
@@ -71,14 +79,25 @@ function removeKey(obj, key) {
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {
-    return {...obj1,...obj2}
-}
+// function combine(obj1, obj2) {
+//     return {...obj1,...obj2}
+// }
+
+const combine = (obj1,obj2) => ({...obj1,...obj2})
 
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {
+// function update(obj, key, val) {
+//     if (obj[key])
+//     {
+//         obj[key] = val
+//         return obj
+//     }
+//     return "Nah"
+// }
+
+const update = (obj, key, val) => {
     if (obj[key])
     {
         obj[key] = val
